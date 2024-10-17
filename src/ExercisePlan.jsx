c8a314517400f3d40a605ceb47e839bd51c4bb40
Aniruddh Nagare <codebyanii@gmail.com>
2024-10-17 10:04:46 -0400

    Remove unused React import in ExercisePlan component

--- src/ExercisePlan.jsx
+++ src/ExercisePlan.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { House } from "lucide-react";
 import Cookies from "js-cookie";  // You'll need to install this package
